8147afbd122c9409d75ee42444bb0d1438ec8e66
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-30 09:12:21 +0400

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -26,6 +26,7 @@ for n_neighbors in neighbors_settings:
     training_accuracy.append(knn.score(X_train, y_train))
     # record test set accuracy
     test_accuracy.append(knn.score(X_test, y_test))
+    
 plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
 plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
 plt.ylabel("Accuracy")
