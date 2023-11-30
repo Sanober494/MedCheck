395dabfacdc35152380fa3b816e3a5a1f8951cc0
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-30 09:12:11 +0400

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -15,6 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.col
 from sklearn.neighbors import KNeighborsClassifier
 training_accuracy = []
 test_accuracy = []
+
 # try n_neighbors from 1 to 10
 neighbors_settings = range(1, 11)
 for n_neighbors in neighbors_settings:
